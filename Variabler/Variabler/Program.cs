2aaee470e8531bb28fc1de3954867ae570db9c66
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-14 14:24:50 +0100

    Fulgt Terje

---
+++ Variabler/Variabler/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Variabler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Terjes undervisning*/
+            Console.Write("Hei, hva heter du? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hei {name}!");
+
+            Console.Write("I hvilket år ble du født? ");
+            string yearStr = Console.ReadLine();
+            int year = Convert.ToInt32(yearStr);
+            int age = 2023 - year;
+            Console.WriteLine($"Da blir du {age} år gammel i år.");
+        }
+    }
+}
