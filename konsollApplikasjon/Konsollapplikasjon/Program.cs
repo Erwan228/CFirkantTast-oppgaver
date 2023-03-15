139a48a3b8ab53a21bc2ca161c9f1672c8c3d061
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-15 13:26:07 +0100

    gjort konsollapplikasjonoppgaven

---
+++ konsollApplikasjon/Konsollapplikasjon/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Konsollapplikasjon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv inn noe og trykk enter ");
+            var rTekst = Console.ReadLine();
+            Console.WriteLine(rTekst);
+            Console.Write(rTekst);
+            Console.WriteLine(rTekst);
+
+            Console.WriteLine("Hei, hva heter du?");
+            var real = Console.ReadLine();
+            Console.WriteLine("Velkommen " + real);
+        }
+    }
+}
